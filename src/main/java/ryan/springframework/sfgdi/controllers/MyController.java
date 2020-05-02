b2104e2c0c58c3ade1a27fadc6e09e20d172bee1
Ryan Tsai <bp4m4h94@gmail.com>
2020-05-02 14:01:47 +0800

    feat: the Spring Context

---
+++ src/main/java/ryan/springframework/sfgdi/controllers/MyController.java
@@ -0,0 +1,15 @@
+package ryan.springframework.sfgdi.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public String sayHello() {
+
+        System.out.println("Hello World !!!");
+
+        return "Say Hello Ended !!!";
+    }
+
+}
